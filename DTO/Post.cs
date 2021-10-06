42529fa5a58a4b3fea3b318117eaa87f0d24b9f1
Stas <tymniaks@gmail.com>
2021-10-06 23:20:32 +0300

    mongoDB

---
+++ DTO/Post.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Runtime;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Post
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public ObjectId userId { get; set; }
+        public string Text { get; set; }
+        public DateTime insertTime { get; set; }
+        public List<ObjectId> CommentsId { get; set; }
+        public List<ObjectId> UserLikesId { get; set; }
+
+    }
+}
