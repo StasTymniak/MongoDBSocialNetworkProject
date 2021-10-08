5d480d446f9ff3e1b04b2adfa6b3749aa826bc85
Stas <tymniaks@gmail.com>
2021-10-08 12:20:45 +0300

    neo4jclient nuGet

---
+++ neo4jConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace neo4jConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
